6dfd94abad7aeaf2e11a5c30c7e80cde9e8ef936
AMANDEEP VIRDHI <amanvirdhi123@gmail.com>
2024-10-20 21:53:28 +0530

    new commit

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.decorators import api_view # type: ignore
+from rest_framework.response import Response # type: ignore
+from rest_framework import status # type: ignore
 from .models import Grievance, User
 from .serializers import GrievanceSerializer, SignupSerializer, UserSerializer
 
